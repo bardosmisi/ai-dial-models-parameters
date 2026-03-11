f101860680d626376dd38e34bab2bb96a241a2df
bardosmisi <bardosmisi@gmail.com>
2026-03-11 16:59:28 +0100

    complete task 2

--- task/1-task-models.py
+++ task/1-task-models.py
@@ -8,11 +8,11 @@ from task.app.main import run
 
 # Models to try:
 # - gpt-4o
-# - claude-3-7-sonnet@20250219
+# - claude-3-7-sonnet@20250219 was unavailable I used claude-3-5-haiku@20241022 instead
 # - gemini-2.5-pro
 
 run(
-    deployment_name='INSERT_DEPLOYMENT_NAME',
+    deployment_name='claude-3-5-haiku@20241022',
     print_request=False, # Switch to False if you do not want to see the request in console
     print_only_content=False, # Switch to True if you want to see only content from response
 )
